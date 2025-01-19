0219d46bfc03bf8a34358e1784e50e05719da362
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2025-01-20 00:34:39 +0530

    Create route.ts

---
+++ app/api/restaurant/onboarding/route.ts
@@ -0,0 +1,57 @@
+import { authMiddleware } from "@/app/lib/middleware/authMiddleware";
+import { restaurantOnboardingSchema } from "@/zod";
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/app/lib/prisma";
+
+export async function POST(req: NextRequest) {
+    console.log("Handler initiated");
+
+    const authResult = await authMiddleware(req);
+
+    if (authResult.error) {
+        return authResult.error; // Return the error response from the middleware
+    }
+
+    const userId = req.headers.get("userId");
+
+    console.log("Extracted userId from headers:", userId);
+
+    if (!userId) {
+        return NextResponse.json({ msg: "User ID not found" }, { status: 500 });
+    }
+
+    const body = await req.json();
+    console.log("Incoming request body:", body);
+
+    const { success } = restaurantOnboardingSchema.safeParse(body);
+
+    if (!success) {
+        return NextResponse.json({ msg: "Invalid data sent" }, { status: 409 });
+    }
+
+    try {
+        const restaurantDetails = await prisma.restaurantDetail.create({
+            data: {
+                restaurantName: body.restaurantName,
+                contactNumber: body.contactNumber,
+                location: body.location,
+                weekdaysWorking: body.WeekdaysWorking,
+                weekendWorking: body.WeekendWorking,
+                logo: body.Logo,
+                instagram: body.Instagram,
+                facebook: body.Facebook,
+                userId: parseInt(userId),
+            },
+        });
+
+        return NextResponse.json(restaurantDetails, { status: 201 });
+    } catch (dbError) {
+        // Ensure dbError is an object before logging
+        if (dbError instanceof Error) {
+            console.error("Database error:", dbError.message);
+        } else {
+            console.error("Database error:", dbError);
+        }
+        return NextResponse.json({ msg: "Internal server error" }, { status: 500 });
+    }
+}
