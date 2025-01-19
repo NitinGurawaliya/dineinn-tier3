773c025f9a79d8ee2410decb138ddfb966f76c56
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2025-01-20 00:34:04 +0530

    Update route.ts

--- app/api/auth/signin/route.ts
+++ app/api/auth/signin/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import prisma from "@/lib/prisma";
+import prisma from "@/app/lib/prisma";
 import { signinSchema } from "@/zod";
 import { sign } from "jsonwebtoken";
 
