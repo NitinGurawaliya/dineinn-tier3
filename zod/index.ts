ddcf8300f356cad7ad5a25104b4b6ac852083426
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2025-01-20 00:34:21 +0530

    Update index.ts

--- zod/index.ts
+++ zod/index.ts
@@ -12,3 +12,13 @@ export const signinSchema = zod.object({
     password:zod.string().min(6)
 })
 
+export const restaurantOnboardingSchema = zod.object({
+    restaurantName :zod.string(),
+    contactNumber :zod.string(),
+    location:zod.string(),
+    WeekendWorking:zod.string().optional(),
+    WeekdaysWorking :zod.string().optional(),
+    Logo:zod.string().optional(),              
+    Instagram:zod.string().optional(),
+    Facebook:zod.string().optional()
+})
