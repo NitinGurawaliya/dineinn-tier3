95d955129eee62fcc3289b23b3ca4e14f286af2f
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2025-01-20 00:34:48 +0530

    Create route.ts

---
+++ app/api/menu/[id]/route.ts
@@ -0,0 +1,39 @@
+import { authMiddleware } from "@/app/lib/middleware/authMiddleware";
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/app/lib/prisma";
+
+export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
+    // Extract the 'id' from params
+    const { id } = params;
+
+    // Check if 'id' is defined
+    if (!id) {
+        return NextResponse.json({ msg: "Menu ID is required" }, { status: 400 });
+    }
+
+    try {
+        // Fetch the menu using Prisma
+        const menu = await prisma.restaurantDetail.findUnique({
+            where: {
+                id: parseInt(id), // Convert to integer
+            },
+            select: {
+                restaurantName: true,
+                logo: true,
+                categories: true,
+                dishes: true,
+            },
+        });
+
+        // Check if the menu exists
+        if (!menu) {
+            return NextResponse.json({ msg: "Menu not found" }, { status: 404 });
+        }
+
+        return NextResponse.json(menu, { status: 200 });
+    } catch (error) {
+        console.error("Error fetching menu:", error);
+        return NextResponse.json({ msg: "Internal server error" }, { status: 500 });
+    }
+}
+
