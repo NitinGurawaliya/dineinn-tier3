00c76080dbb204a26dce8bf62ee85b1f9bf16381
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2025-01-27 16:35:19 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      hello
+      <div>
+      This is landing page 
+      </div>
+      <Link href="/onboarding/auth/signup">
+      <button className="m-2">Signup</button>
+      </Link>
+      <Link href="/">
+      <button>Signin</button>
+      </Link>
+
     </div>
   );
 }
