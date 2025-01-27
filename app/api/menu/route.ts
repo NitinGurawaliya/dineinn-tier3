45c892a397db06dfe74beeb0d232c8beaa4e7482
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2025-01-27 16:35:54 +0530

    Create route.ts

---
+++ app/api/menu/route.ts
@@ -0,0 +1,40 @@
+import { authMiddleware } from "@/app/lib/middleware/authMiddleware";
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/app/lib/prisma";
+
+
+export async function GET(req:NextRequest,res:NextResponse) {
+
+    const authResult = await authMiddleware(req);
+    if(authResult.error){
+        return authResult.error;
+    }
+
+    const userId = req.headers.get("userId");
+
+    if(!userId){
+        return NextResponse.json({msg:"no user id"})
+    }
+
+    console.log("this is response from",userId);
+
+    const response = await prisma.restaurantDetail.findUnique({
+        where:{
+            id:parseInt(userId)
+        },
+        select:{
+            logo:true,
+            restaurantName:true,
+            weekdaysWorking:true,
+            weekendWorking:true,
+            instagram:true,
+            facebook:true,
+            categories:true,
+            dishes:true,
+
+        }
+    })
+
+    return NextResponse.json(response)
+    
+}
