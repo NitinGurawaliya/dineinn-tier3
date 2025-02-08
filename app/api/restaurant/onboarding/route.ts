2d972574e056405c7a943522888d06aebf4a0351
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2025-02-08 20:45:00 +0530

    Update route.ts

--- app/api/restaurant/onboarding/route.ts
+++ app/api/restaurant/onboarding/route.ts
@@ -2,6 +2,15 @@ import { authMiddleware } from "@/app/lib/middleware/authMiddleware";
 import { restaurantOnboardingSchema } from "@/zod";
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/app/lib/prisma";
+import { v2 as cloudinary } from "cloudinary";
+
+// Cloudinary Configuration
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
 
 export async function POST(req: NextRequest) {
     console.log("Handler initiated");
@@ -19,27 +28,41 @@ export async function POST(req: NextRequest) {
     if (!userId) {
         return NextResponse.json({ msg: "User ID not found" }, { status: 500 });
     }
+    const formData = await req.formData();
 
-    const body = await req.json();
-    console.log("Incoming request body:", body);
+    const restaurantName= formData.get("restaurantName") as string
+    const contactNumber = formData.get("contactNumber") as string
+    const location = formData.get("location") as string 
+    const weekdaysWorking = formData.get("weekdaysWorking") as string 
+    const weekendWorking = formData.get("weekendWorking") as string
+    const file = formData.get("logo") as File
+    const instagram = formData.get("instagram") as string
+    const facebook = formData.get("facebook") as string 
 
-    const { success } = restaurantOnboardingSchema.safeParse(body);
 
-    if (!success) {
-        return NextResponse.json({ msg: "Invalid data sent" }, { status: 409 });
-    }
+    // Convert file to base64
+    const buffer = await file.arrayBuffer();
+    const base64String = Buffer.from(buffer).toString("base64");    
+
+    const uploadResponse = await cloudinary.uploader.upload(`data:image/jpeg;base64,${base64String}`, {
+      folder: "dishes_image",
+      public_id: file.name.split(".")[0],
+    });
+
+    const imageUrl = uploadResponse.secure_url;
 
     try {
         const restaurantDetails = await prisma.restaurantDetail.create({
             data: {
-                restaurantName: body.restaurantName,
-                contactNumber: body.contactNumber,
-                location: body.location,
-                weekdaysWorking: body.WeekdaysWorking,
-                weekendWorking: body.WeekendWorking,
-                logo: body.Logo,
-                instagram: body.Instagram,
-                facebook: body.Facebook,
+               
+                restaurantName,
+                contactNumber,
+                location,
+                weekdaysWorking,
+                weekendWorking,
+                logo:imageUrl,
+                instagram,
+                facebook,
                 userId: parseInt(userId),
             },
         });
