be8db903e02078b2c9f49b11e0affa10481b31af
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2025-02-12 22:12:12 +0530

    Update route.ts

--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -3,7 +3,7 @@ import { sign } from "jsonwebtoken";
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/app/lib/prisma";
 
-export async function POST(req: Request,res:Response) {
+export async function POST(req: NextRequest,res:NextResponse) {
   try {
     const body = await req.json();
 
