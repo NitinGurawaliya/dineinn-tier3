db829444cd711e620a8f5745b85108037100f96a
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2025-02-13 00:55:40 +0530

    Update CategoryForm.tsx

--- components/CategoryForm.tsx
+++ components/CategoryForm.tsx
@@ -9,7 +9,7 @@ const CategoryForm = ()=>{
 
     async function categoryHandler() {
         try {
-            const res = await axios.post("http://localhost:3000/api/menu/category", {
+            const res = await axios.post(`https://dineinn-tier2.vercel.app/api/menu/category`, {
                 category: category  
             }, {
                 withCredentials: true  
