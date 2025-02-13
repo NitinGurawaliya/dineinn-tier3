9ac703f257debaa55d1b51841c77e3ce212e05fd
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2025-02-13 18:31:19 +0530

    Update page.tsx

--- app/restaurant/dashboard/page.tsx
+++ app/restaurant/dashboard/page.tsx
@@ -38,7 +38,7 @@ export default async function Dashboard({ searchParams }: { searchParams: { sect
       content = <EditMenu />;
       break;
     case "generate-qr":
-      content = <GenerateQRCode qrData={null} />;
+      content = <GenerateQRCode />;
       break;
     default:
       content = (
