1928c319c691507caebd18c9d93b451587cde18c
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2025-02-15 19:51:40 +0530

    Update Signin.tsx

--- components/Signin.tsx
+++ components/Signin.tsx
@@ -3,18 +3,21 @@
 import { REQUEST_URL } from "@/config";
 import axios from "axios";
 import { use, useState } from "react"
+import { useRouter } from "next/navigation";
 
 export default function SigninComponent(){
     const[password,setPassword] = useState("");
     const[email,setEmail] = useState("")
+    const router  = useRouter()
 
-    async function signupHandler() {
+    async function signinHandler() {
         const res = await axios.post(`${REQUEST_URL}/api/auth/signin`,{
             password,
             email
         })
 
         const data = res.data.token;
+        router.push("/restaurant/dashboard")
         console.log(data)
         
     }
@@ -56,7 +59,7 @@ export default function SigninComponent(){
                           <label  className="font-light text-gray-500 dark:text-gray-300">I accept the <a className="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>
                         </div>
                     </div>
-                    <button onClick={signupHandler} className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
+                    <button onClick={signinHandler} className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
                     <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                         Already have an account? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a>
                     </p>
