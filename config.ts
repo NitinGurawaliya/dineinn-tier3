4331b2043113da703a3dd35bb0dd393373a23afe
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2025-02-28 23:10:43 +0530

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,5 +1,6 @@
-export const REQUEST_URL ="https://dineinn-tier2-iota.vercel.app/"
+export const REQUEST_URL ="https://dineinn-tier2-iota.vercel.app"
 
 //https://dineinn-tier2.vercel.app
 
+//https://dineinn-tier2-iota.vercel.app/
 //http://localhost:3000
