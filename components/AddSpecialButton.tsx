1e6c76d1e7b72bf2ff28b2b5b677408e5649639d
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2025-02-28 15:54:12 +0530

    Update AddSpecialButton.tsx

--- components/AddSpecialButton.tsx
+++ components/AddSpecialButton.tsx
@@ -1,13 +1,19 @@
+"use client"
+
+import { useState } from "react";
 import { AddSpecialDialog } from "./AddSpecialDialog";
 
 export default function AddSpecialButton  (){
+    const[isDishModalOpen,setIsDishModalOpen] = useState(false)
 
     return <div>
-        <button className="px-6 py-3 bg-gray-800 text-white font-semibold rounded-lg shadow-md hover:bg-gray-200 hover:text-black focus:outline-none focus:ring-2 focus:ring-gray-900 focus:ring-opacity-75 transition duration-300 ease-in-out transform hover:scale-105">
+        <button onClick={()=>{
+            setIsDishModalOpen(true)
+        }} className="px-6 py-3 bg-gray-800 text-white font-semibold rounded-lg shadow-md hover:bg-gray-200 hover:text-black focus:outline-none focus:ring-2 focus:ring-gray-900 focus:ring-opacity-75 transition duration-300 ease-in-out transform hover:scale-105">
                 Add Today's Special
               </button>
 
-              {/* <AddSpecialDialog isOpen /> */}
+              <AddSpecialDialog isOpen={isDishModalOpen} onClose={()=>{setIsDishModalOpen(false)}} />
     </div>
 
 }
