87b8497a9b2424aeb37e28a7a3a7d1f49071088a
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2025-03-09 00:24:35 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
     images: {
     domains: ['res.cloudinary.com'], // Add your Cloudinary domain here
   },
+  reactStrictMode: false, // Disable Strict Mode
 };
 
 export default nextConfig;
