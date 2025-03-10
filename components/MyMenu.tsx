22960510f664d23c4e2002d5c5e2031146ea90ca
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2025-03-10 23:58:28 +0530

    Update MyMenu.tsx

--- components/MyMenu.tsx
+++ components/MyMenu.tsx
@@ -77,7 +77,7 @@ export default async function MenuPage() {
              {menuData?.categories && <CategoryComponent  categories={menuData.categories} />} */}
 
             {/* Dishes List */}
-            <div className="grid grid-cols-1 bg-gray-100 p-4 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
+            <div className="grid grid-cols-1 bg-gray-100 p-4 md:grid-cols-2 lg:grid-cols-2 gap-4 mt-4">
                 {menuData?.dishes.map((dish) => (
                     <DishesCard
                     key={dish.id}
