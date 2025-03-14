5b5d86567ad44152c173faa7a2f046c42b518291
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2025-03-14 22:50:59 +0530

    Update route.ts

--- app/api/menu/analytics/dish/[id]/route.ts
+++ app/api/menu/analytics/dish/[id]/route.ts
@@ -1,5 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/app/lib/prisma";
+import { authMiddleware } from "@/app/lib/middleware/authMiddleware";
+import { error } from "console";
+import { cookies } from "next/headers";
 
 
 export async function POST(req:NextRequest,context:{params:{id:string}}) {
