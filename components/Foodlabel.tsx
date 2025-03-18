179da1bc79a788597e2f06cdb439d651eb3eddc5
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2025-03-19 01:56:36 +0530

    Create Foodlabel.tsx

---
+++ components/Foodlabel.tsx
@@ -0,0 +1,21 @@
+import { Circle } from "lucide-react"
+import { Triangle } from "lucide-react"
+
+export function VegLabel() {
+  return (
+    <div className="flex items-center justify-center w-4 h-4 border border-green-600 bg-white">
+      <Circle size={2} className="w-2 h-2 fill-green-600 text-green-600" />
+    </div>
+  )
+}
+
+
+
+export function NonVegLabel() {
+  return (
+    <div className="flex items-center justify-center w-4 h-4 border border-red-800 bg-white">
+      <Triangle  className=" w-2 h-2 fill-red-800 text-red-800" />
+    </div>
+  )
+}
+
