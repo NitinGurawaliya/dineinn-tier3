2dd44f739015c34736d27e2f59a67fec6dcd7d0b
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2025-04-06 13:15:19 +0530

    Update Foodlabel.tsx

--- components/Foodlabel.tsx
+++ components/Foodlabel.tsx
@@ -13,8 +13,8 @@ export function VegLabel() {
 
 export function NonVegLabel() {
   return (
-    <div className="flex items-center justify-center w-4 h-4 border border-red-800 bg-white">
-      <Triangle  className=" w-2 h-2 fill-red-800 text-red-800" />
+    <div className="flex items-center justify-center w-6 h-6 border border-red-800 bg-white">
+      <Triangle  className=" w-3 h-3 fill-red-800 text-red-800" />
     </div>
   )
 }
