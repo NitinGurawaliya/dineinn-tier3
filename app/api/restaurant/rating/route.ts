2bf7bd3ede4e3cdb930120acd489fa237da635df
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2025-04-12 23:25:42 +0530

    Update route.ts

--- app/api/restaurant/rating/route.ts
+++ app/api/restaurant/rating/route.ts
@@ -3,6 +3,9 @@ import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/app/lib/prisma";
 
 
+// implement get rating posted by all the users for restaurants in stars and message optional 
+// this also needs to implemented to show all the rating on the admin dashboard
+
 export  async function POST(req:NextRequest){
 
     const body  = await req.json()
@@ -12,12 +15,4 @@ export  async function POST(req:NextRequest){
     if(!success){
         return NextResponse.json({ msg: "Invalid data" }, { status: 401 });
     }
-
-
-
-    
-
-    
-
-
 }
