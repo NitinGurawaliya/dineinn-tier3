722640d88f9544014603162a7435ffe1b282b7a9
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2025-04-29 00:59:34 +0530

    Update CategoryBar.tsx

--- components/CategoryBar.tsx
+++ components/CategoryBar.tsx
@@ -75,7 +75,7 @@ export default function CategoryComponent({ categories, onCategorySelect }: Cate
     <>
       <div
         ref={categoryRef}
-        className={`bg-transparent mb-2 w-full z-10 ${isSticky ? "fixed top-0 py-2 left-0 shadow-md" : "bg-transparent"}`}
+        className={`bg-transparent mb-2 w-full z-10 ${isSticky ? "fixed top-0 py-2 left-0 " : "bg-transparent"}`}
         id="category-bar"
       >
         <div
