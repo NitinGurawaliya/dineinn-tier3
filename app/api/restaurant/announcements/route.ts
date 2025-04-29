cfa01540b1a346ad019099c4dcdb120565970554
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2025-04-30 01:44:17 +0530

    Create route.ts

---
+++ app/api/restaurant/announcements/route.ts
@@ -0,0 +1,50 @@
+import { authMiddleware } from "@/app/lib/middleware/authMiddleware";
+import { AnnouncementSchema } from "@/zod";
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/app/lib/prisma";
+
+
+
+export async function POST(req:NextRequest) {
+try {
+    
+    const authResult = await authMiddleware(req);
+
+    if (authResult.error) {
+        return authResult.error;
+    }
+
+    const restaurantId = req.cookies.get("userId")?.value;
+
+    if (!restaurantId) {
+        return NextResponse.json({ msg: "User ID or Restaurant ID not found" }, { status: 400 });
+    }
+
+    const body = await req.json();
+
+    const {success} = AnnouncementSchema.safeParse(body)
+
+    if(!success){
+        return NextResponse.json({msg:"Send valid data "},{status:400})
+    }
+
+    const announcement = await prisma.announcement.create({
+        data:{
+            title:body.title,
+            content:body.content,
+            restaurantId:parseInt(restaurantId)
+
+        }
+    })
+
+    console.log("successfully created announcement")
+    return NextResponse.json(announcement);
+
+
+
+} catch (error) {
+    console.log(error)
+    return NextResponse.json({msg:"Internal server err"},{status:500})
+}
+    
+}
